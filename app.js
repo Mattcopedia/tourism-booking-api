d651ab15fb9a06d2bf4416618d9820079ca29984
Mattcopedia <mathiasofeimun@gmail.com>
2026-03-26 13:39:59 +0100

    prepared app for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -109,7 +110,9 @@ app.use(
       'price'
     ]
   })
-);
+); 
+//only works to compress text
+app.use(compression());
 
 //Test Middleware
 app.use((req, res, next) => {
